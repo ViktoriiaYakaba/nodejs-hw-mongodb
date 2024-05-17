12e561c373262dfb98ca9c49307c4cc9fd4ab85c
ViktoriiaYakaba <yakaba@bk.ru>
2024-05-17 18:57:44 +0200

    Update contacts.js

--- src/validation/contacts.js
+++ src/validation/contacts.js
@@ -1,7 +1,7 @@
 import Joi from 'joi';
 
 export const creaContactSchema = Joi.object({
-    name: Joi.string().min(3).max(30).required().messages({
+    name: Joi.string().min(3).max(20).required().messages({
         'string.base': 'Username should be a string',
         'string.min': 'Username should have at least {#limit} characters',
         'string.max': 'Username should have at most {#limit} characters',
@@ -10,18 +10,19 @@ export const creaContactSchema = Joi.object({
     phoneNumber: Joi.string().required(),
     email: Joi.string().email().required(),
     isFavourite: Joi.boolean().required(),
-    contactType: Joi.string().valid('personal', 'work').required(),
+    contactType: Joi.string().valid('work', 'home', 'personal').required(),
     createdAt: Joi.date().iso().required(),
     updatedAt: Joi.date().iso().required(),
 });
 
 export const updateContactSchema = Joi.object({
-  name: Joi.string().min(3).max(30),
-  email: Joi.string().email(),
-  age: Joi.number().integer().min(6).max(16),
-  gender: Joi.string().valid('male', 'female', 'other'),
-  avgMark: Joi.number().min(2).max(12),
-  onDuty: Joi.boolean(),
+    name: Joi.string().min(3).max(20),
+    phoneNumber: Joi.string().required(),
+    email: Joi.string().email().required(),
+    isFavourite: Joi.boolean().required(),
+    contactType: Joi.string().valid('work', 'home', 'personal').required(),
+    createdAt: Joi.date().iso().required(),
+    updatedAt: Joi.date().iso().required(),
 });
 
 
