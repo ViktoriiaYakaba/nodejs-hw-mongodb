92b823f658732fa1e836cc12dfc15aed12f6f919
ViktoriiaYakaba <yakaba@bk.ru>
2024-05-25 16:26:33 +0200

    Update auth.js

--- src/routers/auth.js
+++ src/routers/auth.js
@@ -6,6 +6,7 @@ import { validateBody } from '../middlewares/validateBody.js';
 import { loginUserSchema } from "../validation/auth.js";
 import { loginUserController } from "../controllers/auth.js";
 import { logoutUserController } from "../controllers/auth.js";
+import { refreshUserSessionController } from "../controllers/auth.js";
 
 const router = Router();
 
@@ -15,6 +16,6 @@ router.post('/login', validateBody(loginUserSchema), ctrlWrapper(loginUserContro
 
 router.post('/logout', ctrlWrapper(logoutUserController));
 
-router.post()
+router.post('/refresh', ctrlWrapper(refreshUserSessionController));
 
 export default router;
