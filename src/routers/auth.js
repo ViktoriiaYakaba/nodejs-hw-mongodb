55b3f9dba7b725bb174abc13abb9c829f80639b2
ViktoriiaYakaba <yakaba@bk.ru>
2024-06-06 18:20:56 +0200

    Update auth.js

--- src/routers/auth.js
+++ src/routers/auth.js
@@ -9,6 +9,8 @@ import { logoutUserController } from "../controllers/auth.js";
 import { refreshUserSessionController } from "../controllers/auth.js";
 import { requestResetEmailSchema } from "../validation/auth.js";
 import { requestResetEmailController } from "../controllers/auth.js";
+import { resetPasswordSchema } from "../validation/auth.js";
+import { resetPasswordController } from "../controllers/auth.js";
 
 const router = Router();
 
@@ -20,10 +22,8 @@ router.post('/logout', ctrlWrapper(logoutUserController));
 
 router.post('/refresh', ctrlWrapper(refreshUserSessionController));
 
-router.post(
-  '/request-reset-email',
-  validateBody(requestResetEmailSchema),
-  ctrlWrapper(requestResetEmailController),
-);
+router.post('/request-reset-email', validateBody(requestResetEmailSchema), ctrlWrapper(requestResetEmailController));
+
+router.post('/reset-password', validateBody(resetPasswordSchema), ctrlWrapper(resetPasswordController));
 
 export default router;
