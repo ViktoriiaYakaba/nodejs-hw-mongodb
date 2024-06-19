6303cc282d0deb7435542d248ad562434feddb78
ViktoriiaYakaba <yakaba@bk.ru>
2024-06-19 18:28:52 +0200

    Update google-oauth.json

--- src/routers/auth.js
+++ src/routers/auth.js
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
-import { registerUserSchema } from "../validation/auth.js";
-import { registerUserController } from "../controllers/auth.js";
+import { loginWithGoogleOAuthSchema, registerUserSchema } from "../validation/auth.js";
+import { loginWithGoogleController, registerUserController } from "../controllers/auth.js";
 import { validateBody } from '../middlewares/validateBody.js';
 import { loginUserSchema } from "../validation/auth.js";
 import { loginUserController } from "../controllers/auth.js";
@@ -11,6 +11,9 @@ import { requestResetEmailSchema } from "../validation/auth.js";
 import { requestResetEmailController } from "../controllers/auth.js";
 import { resetPasswordSchema } from "../validation/auth.js";
 import { resetPasswordController } from "../controllers/auth.js";
+import { getGoogleOAuthUrlController } from "../controllers/auth.js";
+import { loginUserSchema } from "../validation/auth.js";
+import { loginUserController } from "../controllers/auth.js";
 
 const router = Router();
 
@@ -25,5 +28,9 @@ router.post('/refresh', ctrlWrapper(refreshUserSessionController));
 router.post('/request-reset-email', validateBody(requestResetEmailSchema), ctrlWrapper(requestResetEmailController));
 
 router.post('/reset-password', validateBody(resetPasswordSchema), ctrlWrapper(resetPasswordController));
+
+router.get('/get-oauth-url', ctrlWrapper(getGoogleOAuthUrlController));
+
+router.post('/confirm-oauth', validateBody(loginWithGoogleOAuthSchema), ctrlWrapper(loginWithGoogleController));
 
 export default router;
